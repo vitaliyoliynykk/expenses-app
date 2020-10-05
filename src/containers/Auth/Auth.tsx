1cb1987dccc7f0f5ccad1d687b4b20135c55892a
Nataliia <nat.leshchak@gmail.com>
2020-10-05 11:32:16 +0300

    added routing, created auth component, button

--- src/containers/Auth/Auth.tsx
+++ src/containers/Auth/Auth.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import './Auth.scss';
 import Logo from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import { LOG_IN } from './Auth.constants';
 
 class Auth extends React.Component {
     render(): React.ReactElement {
         return (
-            <>
-                <div className="container-auth">
-                    <img src={Logo} alt="logo" />
-                    <div className="container-auth__title">EXPENSES APP</div>
-                    <button className="container-auth__btn container-auth__btn_log-in">LOG IN</button>
-                    <Link to="/about" className="container-auth__btn container-auth__btn_about">
+            <div className="container-auth">
+                <img src={Logo} alt="logo" />
+                <div className="container-auth__title">EXPENSES APP</div>
+                <Button title={LOG_IN} />
+                <Router>
+                    <Link to="/about" className="container-auth__link">
                         ABOUT
                     </Link>
-                    <div className="container-auth__line" />
-                </div>
-            </>
+                </Router>
+                <div className="container-auth__line" />
+            </div>
         );
     }
 }
