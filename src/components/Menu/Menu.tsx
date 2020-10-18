6762ceebfb16da29b5155b23511547cc32b61917
Nataliia Leshchak <nataliia.leshchak@voypost.com>
2020-10-18 22:27:30 +0300

    added category icon, changes one function

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,15 +2,48 @@ import React from 'react';
 import './Menu.scss';
 import Add from '../../assets/plus.svg';
 import Chart from '../../assets/profits.svg';
+import Category from '../../assets/category.svg';
 import Settings from '../../assets/settings.svg';
-import { MenuProps } from '../../models/models';
+import { useHistory } from 'react-router-dom';
 
-const Menu = ({ handleClickOnChart, handleClickOnAdd, handleClickOnSettings }: MenuProps): React.ReactElement => {
+const Menu = (): React.ReactElement => {
+    const history = useHistory();
+
+    const handleClick = (action: string): void => {
+        switch (action) {
+            case 'chart':
+                history.push('/chart');
+                break;
+            case 'add':
+                history.push('/add');
+                break;
+            case 'category':
+                history.push('/category');
+                break;
+            case 'settings':
+                history.push('/settings');
+                break;
+            default:
+                break;
+        }
+    };
+
     return (
         <div className="container-menu">
-            <img src={Chart} alt="profits" className="container-menu__img" onClick={handleClickOnChart} />
-            <img src={Add} alt="add" className="container-menu__img" onClick={handleClickOnAdd} />
-            <img src={Settings} alt="settings" className="container-menu__img" onClick={handleClickOnSettings} />
+            <img src={Chart} alt="profits" className="container-menu__img" onClick={(): void => handleClick('chart')} />
+            <img src={Add} alt="add" className="container-menu__img" onClick={(): void => handleClick('add')} />
+            <img
+                src={Category}
+                alt="category"
+                className="container-menu__img"
+                onClick={(): void => handleClick('category')}
+            />
+            <img
+                src={Settings}
+                alt="settings"
+                className="container-menu__img"
+                onClick={(): void => handleClick('settings')}
+            />
         </div>
     );
 };
--- src/models/models.ts
+++
@@ -1,5 +0,0 @@
-export interface MenuProps {
-    handleClickOnChart?: () => void;
-    handleClickOnAdd?: () => void;
-    handleClickOnSettings?: () => void;
-}
