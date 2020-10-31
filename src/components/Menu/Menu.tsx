45b4c2621718f980c50e61769d5296521d348c01
Nataliia Leshchak <nataliia.leshchak@voypost.com>
2020-10-31 15:21:33 +0200

    added dashboard icon, pass function up

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,42 +2,25 @@ import React from 'react';
 import './Menu.scss';
 import Add from '../../assets/plus.svg';
 import Chart from '../../assets/profits.svg';
-import Category from '../../assets/category.svg';
+import Dashboard from '../../assets/dashboard.svg';
 import Settings from '../../assets/settings.svg';
-import { useHistory } from 'react-router-dom';
+import { MenuProps } from '../../models/models';
 
-const Menu = (): React.ReactElement => {
-    const history = useHistory();
-
+const Menu = ({ onActionClick }: MenuProps): React.ReactElement => {
     const handleClick = (action: string): void => {
-        switch (action) {
-            case 'chart':
-                history.push('/chart');
-                break;
-            case 'add':
-                history.push('/add');
-                break;
-            case 'category':
-                history.push('/category');
-                break;
-            case 'settings':
-                history.push('/settings');
-                break;
-            default:
-                break;
-        }
+        onActionClick(action);
     };
 
     return (
         <div className="container-menu">
+            <img
+                src={Dashboard}
+                alt="dashboard"
+                className="container-menu__img"
+                onClick={(): void => handleClick('dashboard')}
+            />
             <img src={Chart} alt="profits" className="container-menu__img" onClick={(): void => handleClick('chart')} />
             <img src={Add} alt="add" className="container-menu__img" onClick={(): void => handleClick('add')} />
-            <img
-                src={Category}
-                alt="category"
-                className="container-menu__img"
-                onClick={(): void => handleClick('category')}
-            />
             <img
                 src={Settings}
                 alt="settings"
--- src/models/models.ts
+++ src/models/models.ts
@@ -1,3 +1,7 @@
 export interface ButtonProps {
     title: string;
 }
+
+export interface MenuProps {
+    onActionClick: (action: string) => void;
+}
