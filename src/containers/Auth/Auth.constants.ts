8abbf1734ab64f70e0233670e7e9852af221be70
Vitalii <vitaliyoliynykk@gmail.com>
2020-11-09 18:07:04 +0200

    setup redux, set user to store

--- src/containers/Auth/Auth.constants.ts
+++ src/containers/Auth/Auth.constants.ts
@@ -1,1 +1,14 @@
+import firebase from 'firebase';
+
 export const ABOUT = 'ABOUT';
+export const UI_CONFIG = {
+    signInFlow: 'popup',
+    signInSuccessUrl: '/',
+    signInOptions: [
+        {
+            provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            fullLabel: 'LOG IN',
+            buttonColor: '#131313',
+        },
+    ],
+};
